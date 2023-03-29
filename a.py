d1d0ff9262f1a2b31757838f9b62966bc5c468d8
Ashish chaudhary <36606232+ashishc628@users.noreply.github.com>
2023-03-29 12:23:33 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
-!pip install pyrebase
-!pip install streamlit
-!pip install pycryptodome==3.17
+pip install pyrebase
+pip install streamlit
+pip install pycryptodome==3.17
 # Modules
 import pyrebase
 import streamlit as st
